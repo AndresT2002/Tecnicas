a12d1b21e8641d18c1f182441d59be3a43800c9c
Andres Toloza <andres.toloza@udea.edu.co>
2022-04-03 16:03:22 -0500

    conectado final a bd

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose');
-const url='mongodb+srv://atoloza:<Memoriasuper123>@cluster0.wknwa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const url='mongodb+srv://atoloza123:Memoriasuper123@cluster0.wknwa.mongodb.net/tecnicas'
 
 
 mongoose.connect(url,{
